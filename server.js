aa00d2c352fc17d56f18c3a6bdb247ff12525614
Nikhithareddy45 <nikkireddyperugu@gmail.com>
2026-01-14 18:07:05 +0530

    Deploy / check

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("GigFlow API is running 🚀");
+});
